366b9535c61a6f3db179270a254c2a0f764b035b
Olga Reva <reva.olga@gmail.com>
2013-08-01 13:19:05 +0300

    Creation of TaskQueRunners moved into the constructor of TaskRunnerImpl2.

--- Homework10_Concurrency/src/main/java/ua/org/oreva/studyNN/runner/TaskRunnerImpl2.java
+++ Homework10_Concurrency/src/main/java/ua/org/oreva/studyNN/runner/TaskRunnerImpl2.java
@@ -14,26 +14,22 @@ import java.util.ArrayList;
 public class TaskRunnerImpl2 implements TaskRunner {
 	private TaskQueue taskQueue;
 	private ArrayList<Thread> taskQueueRunners;
-	private int numOfRunners;
 
 	public TaskRunnerImpl2(int numberOfThreads) {
-		numOfRunners = numberOfThreads;
-		taskQueueRunners = new ArrayList<Thread>(numOfRunners);
+		taskQueueRunners = new ArrayList<Thread>(numberOfThreads);
 		taskQueue = new TaskQueue();
+		// Create all runners
+		for (int i = 0; i < numberOfThreads; i++) {
+			String threadName = "Thread T" + String.valueOf(i);
+			TaskQueueRunner runner = new TaskQueueRunner(taskQueue, threadName);
+			Thread t = new Thread(runner);
+			taskQueueRunners.add(t);
+			t.start();
+		}
 	}
 
 	@Override
 	public <X, Y> X run(Task<X, Y> task, Y value) throws InterruptedException {
-		// Add new runner if possible
-		synchronized (taskQueueRunners) {
-			if (taskQueueRunners.size() < numOfRunners) {
-				String threadName = "Thread T" + String.valueOf(taskQueueRunners.size());
-				TaskQueueRunner runner = new TaskQueueRunner(taskQueue, threadName);
-				Thread t = new Thread(runner);
-				taskQueueRunners.add(t);
-				t.start();
-			}
-		}
 		// Add task to queue
 		taskQueue.put(task);
 		return null; //Real task result we'll see after queue processing (TaskQueueRunner will write it to the screen).
